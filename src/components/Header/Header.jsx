7d8460fe167bfd06180482befcd4cb6943a666e2
dong-geon-Lee <ch9716@naver.com>
2023-04-04 18:26:55 +0900

    Hedaer 컴포넌트 스타일 컴포넌트 리팩토링

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ANSWER__RANGE__TEXT, RETRY__BTN } from "../../constants/constants";
-import { Container, Button, Text } from "./styles";
+import * as S from "./styles";
 
 const Header = ({ handleGameReset }) => {
   return (
-    <Container>
-      <Button onClick={() => handleGameReset()}>{RETRY__BTN}</Button>
-      <Text>{ANSWER__RANGE__TEXT}</Text>
-    </Container>
+    <S.Container>
+      <S.Button onClick={() => handleGameReset()}>{RETRY__BTN}</S.Button>
+      <S.Text>{ANSWER__RANGE__TEXT}</S.Text>
+    </S.Container>
   );
 };
 
