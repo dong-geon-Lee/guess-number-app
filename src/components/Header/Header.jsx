4d7f26623c3b1b351f2ad601d7bc0ce82d3633b2
dong-geon-Lee <ch9716@naver.com>
2023-04-04 18:33:37 +0900

    Header 컴포넌트 상수 리팩토링

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ANSWER__RANGE__TEXT, RETRY__BTN } from "../../constants/constants";
 import * as S from "./styles";
+import * as C from "../../constants/constants";
 
 const Header = ({ handleGameReset }) => {
   return (
     <S.Container>
-      <S.Button onClick={() => handleGameReset()}>{RETRY__BTN}</S.Button>
-      <S.Text>{ANSWER__RANGE__TEXT}</S.Text>
+      <S.Button onClick={() => handleGameReset()}>{C.RETRY__BTN}</S.Button>
+      <S.Text>{C.ANSWER__RANGE__TEXT}</S.Text>
     </S.Container>
   );
 };
