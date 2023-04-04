c081389d4de8fbff585476685503ac90006860bc
dong-geon-Lee <ch9716@naver.com>
2023-04-04 18:32:01 +0900

    Contents 컴포넌트 내부 상수 리팩토링

--- src/components/Main/Contents/Contents.jsx
+++ src/components/Main/Contents/Contents.jsx
@@ -1,25 +1,21 @@
 import React from "react";
-import {
-  GAME__OVER,
-  MEDAL__ICON,
-  SCORE__ICON,
-} from "../../../constants/constants";
 import * as S from "./styles";
+import * as C from "../../../constants/constants";
 
 const Contents = ({ text, score, highScore }) => {
-  const displayGameStatus = score > 0 ? text : GAME__OVER;
+  const displayGameStatus = score > 0 ? text : C.GAME__OVER;
 
   return (
     <S.Container>
       <S.Text className="media__h2">{displayGameStatus}</S.Text>
       <S.Content>
         <S.Div>
-          <S.Icon>{SCORE__ICON}</S.Icon>
+          <S.Icon>{C.SCORE__ICON}</S.Icon>
           <S.Label>Score:</S.Label>
           <S.Text>{score}</S.Text>
         </S.Div>
         <S.Div>
-          <S.Icon>{MEDAL__ICON}</S.Icon>
+          <S.Icon>{C.MEDAL__ICON}</S.Icon>
           <S.Label>HighScore:</S.Label>
           <S.Text>{highScore}</S.Text>
         </S.Div>
