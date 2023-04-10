4d8c5f81c7b66eb646baec7df751ceebacf48b3d
dong-geon-Lee <ch9716@naver.com>
2023-04-10 23:12:13 +0900

    Actions 컴포넌트 리덕스툴킷 로직 적용

--- src/components/Main/Actions/Actions.jsx
+++ src/components/Main/Actions/Actions.jsx
@@ -1,48 +1,58 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { checkBtnStatus } from "../../../helpers/helpers";
+import {
+  changeText,
+  correctNumber,
+  decreaseNumber,
+  increaseNumber,
+  missNumber,
+} from "../../../reduxToolkit/features/gameSlice";
 import * as S from "./styles";
 import * as C from "../../../constants/constants";
 
-const Actions = ({ number, setNumber, numCheck, handleCheckNumber, score }) => {
-  const playing = score > 0;
-  const btnDisabled = checkBtnStatus(numCheck, playing);
+const Actions = () => {
+  const { number, validIsNumber, score, randomNumber } = useSelector(
+    (state) => state.game
+  );
 
-  const handleMinusNumber = (num) => {
-    if (num <= 0) return;
-    setNumber((prevState) => prevState - 1);
+  const dispatch = useDispatch();
+  const playing = score > 0;
+  const btnDisabled = checkBtnStatus(validIsNumber, playing);
+
+  const handleMinusNumber = () => {
+    if (number <= 1) return;
+    dispatch(decreaseNumber(number));
   };
 
-  const handlePlusNumber = (num) => {
-    if (num >= 10) return;
-    setNumber((prevState) => prevState + 1);
+  const handlePlusNumber = () => {
+    if (number >= 10) return;
+    dispatch(increaseNumber(number));
+  };
+
+  const handleCorrectNumber = () => {
+    if (number !== randomNumber) {
+      if (number > randomNumber) dispatch(changeText(C.HIGH__NUMBER));
+      if (number < randomNumber) dispatch(changeText(C.LOW__NUMBER));
+      dispatch(missNumber());
+      return;
+    }
+
+    const correctGame = { highScore: score, text: C.CORRECT__MESSAGE };
+    dispatch(correctNumber(correctGame));
   };
 
   return (
     <S.Container>
       <S.Border>{number}</S.Border>
       <S.BtnBox>
-        <S.DownBtn
-          onClick={() => handleMinusNumber(number)}
-          check={numCheck}
-          play={playing}
-          disabled={btnDisabled}
-        >
+        <S.DownBtn onClick={handleMinusNumber} disabled={btnDisabled}>
           {C.MINUS__BTN}
         </S.DownBtn>
-        <S.UpBtn
-          onClick={() => handlePlusNumber(number)}
-          check={numCheck}
-          play={playing}
-          disabled={btnDisabled}
-        >
+        <S.UpBtn onClick={handlePlusNumber} disabled={btnDisabled}>
           {C.PLUS__BTN}
         </S.UpBtn>
-        <S.CheckBtn
-          onClick={() => handleCheckNumber(number)}
-          check={numCheck}
-          play={playing}
-          disabled={btnDisabled}
-        >
+        <S.CheckBtn onClick={handleCorrectNumber} disabled={btnDisabled}>
           {C.CHECK__BTN}
         </S.CheckBtn>
       </S.BtnBox>
