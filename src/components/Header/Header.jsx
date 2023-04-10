a54a912b05f6c105bc35725d303e65d399ac0f56
dong-geon-Lee <ch9716@naver.com>
2023-04-10 23:12:50 +0900

    Header 컴포넌트 리셋 액션함수 적용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { gameReset } from "../../reduxToolkit/features/gameSlice";
 import * as S from "./styles";
 import * as C from "../../constants/constants";
 
-const Header = ({ handleGameReset }) => {
+const Header = () => {
+  const dispatch = useDispatch();
+
   return (
     <S.Container>
-      <S.Button onClick={() => handleGameReset()}>{C.RETRY__BTN}</S.Button>
+      <S.Button onClick={() => dispatch(gameReset())}>{C.RETRY__BTN}</S.Button>
       <S.Text>{C.ANSWER__RANGE__TEXT}</S.Text>
     </S.Container>
   );
